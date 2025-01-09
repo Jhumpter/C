c725b46f124c141e3d6da6a488f092c64b6bfe9b
João Paulo <joaopwii@gmail.com>
2025-01-08 22:57:14 -0300

    Update 2157.c

--- Outros/2157.c
+++ Outros/2157.c
@@ -19,16 +19,20 @@ int main(){
             int centenas = 0;
             int milhar = 0;
             int dezmilhar = 0;
+            int casas = 1;
+            if(sequencia[j]<10) unidades = sequencia[j];
             if(sequencia[j]>=10 && sequencia[j]<100){
                 dezenas = sequencia[j]/10;
-                int unidades = sequencia[j]-dezenas*10;
-                sequencia[j] = (unidades*10)+(dezenas);                
+                unidades = sequencia[j]-dezenas*10;
+                sequencia[j] = (unidades*10)+(dezenas);
+                casas = 2;
             }
             else if(sequencia[j]>99 && sequencia[j]<1000){
                 centenas = sequencia[j]/100;
                 dezenas = (sequencia[j]-(centenas*100))/10;
                 unidades = sequencia[j]-(dezenas*10)-(centenas*100);
                 sequencia[j] = (unidades*100)+(dezenas*10)+(centenas);
+                casas = 3;
             }
             else if(sequencia[j]>999 && sequencia[j]<10000){
                 milhar = sequencia[j]/1000;
@@ -36,6 +40,7 @@ int main(){
                 dezenas = (sequencia[j]-(milhar*1000)-(centenas*100))/10;
                 unidades = sequencia[j]-(milhar*1000)-(centenas*100)-(dezenas*10);
                 sequencia[j] = (unidades*1000)+(dezenas*100)+(centenas*10)+milhar;
+                casas = 4;
             }
             else if(sequencia[j]>9999 && sequencia[j]<100000){
                 dezmilhar = sequencia[j]/10000;
@@ -44,17 +49,17 @@ int main(){
                 dezenas = (sequencia[j]-(milhar*1000)-(centenas*100)-(dezmilhar*10000))/100;
                 unidades = sequencia[j]-(milhar*1000)-(centenas*100)-(dezenas*10)-(dezmilhar*10000);
                 sequencia[j] = (unidades*10000)+(dezenas*1000)+(centenas*100)+(milhar*10)+dezmilhar;
+                casas = 5;
             }
             if(unidades==0){
-                if(dezmilhar!=0) printf("00001");
-                else if(milhar!=0) printf("0001");
-                else if(centenas!=0) printf("001");
-                else if(dezenas!=0) printf("01");
-                else printf("1");
+                printf("0");
+                if(dezenas==0 && casas>2) printf("0");
+                if(dezenas==0 && centenas==0 && casas>3) printf("0");
+                if(dezenas==0 && centenas==0 && milhar==0 && casas>4) printf("0");
             } 
-            else printf("%d", sequencia[j]);
+            printf("%d", sequencia[j]);
         }
         printf("\n");
     }
     return 0;
-}
+} //Nao aceitou mas nao mostra porque
